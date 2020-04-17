4fd03c3e5811d3608e0e0204ed319376feab27bb
E.Karthik <kartheekenumarthi@gmail.com>
2020-04-17 18:46:57 +0530

    added projects section

---
+++ js/script.js
@@ -0,0 +1,32 @@
+var swiper = new Swiper('.swiper-container1', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    keyboard: {
+        enabled: true,
+    },
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  var swiper2 = new Swiper('.swiper-container', {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
