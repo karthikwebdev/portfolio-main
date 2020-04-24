664e31b1c777663a4b1c472ec1cdb6bad7f10212
E.Karthik <kartheekenumarthi@gmail.com>
2020-04-24 18:17:53 +0530

    changed video and image size

--- js/script.js
+++ js/script.js
@@ -30,3 +30,22 @@ var swiper = new Swiper('.swiper-container1', {
       prevEl: '.swiper-button-prev',
     },
   });
+
+const navitems =  document.querySelectorAll(".nav-link");
+const sections = document.querySelectorAll("section");
+
+document.querySelector("body").addEventListener("scroll",()=>{
+  let max = sections[0]
+  sections.forEach((section)=>{
+    if(section.offsetTop < Math.floor(document.querySelector("body").scrollTop)+5){
+      max = section.id
+    }
+  })
+
+  navitems.forEach((nav)=>{
+    nav.classList.remove("active")
+    if (max === nav.getAttribute("data-nav")){
+      nav.classList.add("active")
+    }
+  })
+})
